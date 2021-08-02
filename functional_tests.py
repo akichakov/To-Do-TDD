8e09fb3bd7f4946dae63c1d429cf08831d6babdf
AndrewKichakov <ku4akov@gmail.com>
2021-08-02 20:19:44 +0300

    Added new strings in .gitignore, using unittest in FT

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,31 @@
 from selenium import webdriver
+import unittest
 
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    """тест нового посетителя"""
 
-assert 'The install worked successfully! Congratulations!' in browser.title
+    def setUp(self):
+        """установка"""
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        """демонтаж"""
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """тест: можно начать список и получить его позже"""
+        # Эдит слышала про крутое новое онлайн-приложение со
+        # списком неотложных дел. Она решает оценить его
+        # домашнюю страницу
+        self.browser.get('http://localhost:8000')
+        # Она видит, что заголовок и шапка страницы говорят о
+        # списках неотложных дел
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Закончить тест!')
+        # Ей сразу же предлагается ввести элемент списка
+        # [...остальные комментарии, как и прежде]
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
