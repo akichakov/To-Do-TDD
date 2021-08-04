67504d79d07ef149837da62b85ac1c74e29ef717
AndrewKichakov <ku4akov@gmail.com>
2021-08-04 18:37:11 +0300

    New URL, view and template to display lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from lists import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    re_path(r'^lists/one-list-in-the-world/$',
+            views.view_list, name='view_list'),
     path('admin/', admin.site.urls),
 ]
