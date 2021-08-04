651180497b31b67fd03e94109351f8cf95a4a149
AndrewKichakov <ku4akov@gmail.com>
2021-08-04 23:58:12 +0300

    New URL + template for add lists. FT working

--- superlists/urls.py
+++ superlists/urls.py
@@ -5,7 +5,7 @@ from lists import views
 urlpatterns = [
     path('', views.home_page, name='home'),
     re_path(r'^lists/new$', views.new_list, name='new_list'),
-    re_path(r'^lists/one-list-in-the-world/$',
-            views.view_list, name='view_list'),
+    re_path(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+    re_path(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
     path('admin/', admin.site.urls),
 ]
