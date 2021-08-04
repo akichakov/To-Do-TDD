19a33483d3d42fb98534f2e38afa5ee7d48f5a4f
AndrewKichakov <ku4akov@gmail.com>
2021-08-04 20:20:04 +0300

    Added new model List + upgrade test

--- lists/models.py
+++ lists/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+
+
+class List(models.Model):
+    """отдельный список"""
+    pass
 
 
 class Item(models.Model):
     """элемент списка"""
     text = models.TextField(default='')
+    list = models.ForeignKey(List, on_delete=CASCADE, default=None)
